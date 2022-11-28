1175c43c2ca0e2ec6e0c3ac110e0bdbe4071ba20
dcarrillo9181 <27480583+dcarrillo9181@users.noreply.github.com>
2022-11-27 18:44:06 -0800

    Added components and created pages

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+import toast from 'react-hot-toast';
 
 export default function Home() {
   return (
     <div>
-      
+      <Link prefetch={false} href={{
+            pathname: '/[username]',
+            query: { username: 'dorianc85' },
+          }}>
+        <>Dorian's profile</>
+      </Link>
+      <button onClick={() => toast.success('hello toast!')}>
+        Toast Me
+      </button>
     </div>
   )
 }
